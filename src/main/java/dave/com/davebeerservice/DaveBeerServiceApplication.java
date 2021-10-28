9c5dc80600d600056ae3b1d58e732fe3a073b3d5
Dave <dave.harms@fortyau.com>
2021-10-28 14:58:09 -0500

    initial project add. Closes #1

---
+++ src/main/java/dave/com/davebeerservice/DaveBeerServiceApplication.java
@@ -0,0 +1,13 @@
+package dave.com.davebeerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaveBeerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaveBeerServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dave/com/davebeerservice/DaveBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dave.com.davebeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaveBeerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
