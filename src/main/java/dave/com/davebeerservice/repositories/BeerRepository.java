1b43b62a904f21569990f36d281587992f41f1e4
Dave <dave.harms@fortyau.com>
2021-10-30 17:35:47 -0500

    added paging and sorting repository for beer. Closes #6

---
+++ src/main/java/dave/com/davebeerservice/repositories/BeerRepository.java
@@ -0,0 +1,9 @@
+package dave.com.davebeerservice.repositories;
+
+import dave.com.davebeerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}
